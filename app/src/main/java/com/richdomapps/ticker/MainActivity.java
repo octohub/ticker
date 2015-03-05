4c35dd525f15e4d40b7c97c7ec3132af9a88cdc1
Richard Lucas <richard.lucas@raizlabs.com>
2015-03-04 20:37:04 -0500

    Added button method to start full screen activity.

--- app/src/main/java/com/richdomapps/ticker/MainActivity.java
+++ app/src/main/java/com/richdomapps/ticker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.richdomapps.ticker;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -27,7 +28,8 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void fullScreenActivity(View view){
-
+        Intent intent = new Intent(this, FullscreenActivity.class);
+        startActivity(intent);
     }
 
 
